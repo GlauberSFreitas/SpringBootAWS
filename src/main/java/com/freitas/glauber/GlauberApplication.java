c92cc211517e908a20e719827aa18c9a75c895c2
GlauberSFreitas <glaubersfreitas@gmail.com>
2021-02-18 18:51:29 -0300

    primeiro

---
+++ src/main/java/com/freitas/glauber/GlauberApplication.java
@@ -0,0 +1,13 @@
+package com.freitas.glauber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlauberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlauberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freitas/glauber/GlauberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freitas.glauber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlauberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
